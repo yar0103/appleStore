b4752c27429a53339aedbd989e2a28cb2a294686
arin <yurins0103@naver.com>
2024-06-08 17:40:49 +0900

    bootstrap test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from "react-bootstrap";
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
 
 function App() {
   return (
+    
     <div className="App">
-      apple store
+      <Button > btn</Button>
+      <Navbar className='navbar' bg='primary' data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href='#home'>Dessert Shop</Navbar.Brand>
+          <Nav className='me-auto'>
+            <Nav.Link href='#home'>Home</Nav.Link>
+            <Nav.Link href='#features'>Dessert</Nav.Link>
+            <Nav.Link href='#pricing'>Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
