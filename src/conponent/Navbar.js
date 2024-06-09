64e2894ec719ae1ae9db3a1bc65a09c438c73059
arin <yurins0103@naver.com>
2024-06-09 18:37:19 +0900

    complete Completed category creation

--- src/conponent/Navbar.js
+++ src/conponent/Navbar.js
@@ -128,36 +128,250 @@ const Navbar = () => {
           <a href="#">
             iPhone
           </a>
+          <ul>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                iPhone 모두보기
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                iPhone 15 Pro
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                iPhone 15
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                iPhone 14
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                iPhone 13
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                iPhone SE
+              </a>
+            </li>
+          </ul>
         </li>
         <li className="navbarItem">
           <a href="#">
             Watch
           </a>
+          <ul>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                Apple Watch 모두보기
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                Apple Watch Series 9
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                Apple Watch Ultra 2
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                Apple Watch SE
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                Apple Watch Nike
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                Apple Watch Hermes
+              </a>
+            </li>
+          </ul>
         </li>
         <li className="navbarItem">
           <a href="#">
             AirPods
           </a>
+          <ul>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                AirPods 모두보기
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                AirPods Pro 2세대
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                AirPods 2세대
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                AirPods 3세대
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                AirPods Max
+              </a>
+            </li>
+          </ul>
         </li>
         <li className="navbarItem">
           <a href="#">
             TV etc
           </a>
+          <ul>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                TV 및 홈 모두보기
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                Apple TV 4K
+              </a>
+            </li>
+          </ul>
         </li>
         <li className="navbarItem">
           <a href="#">
             Entertainment
           </a>
+          <ul>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                Apple One
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                Apple TV+
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                Apple Music
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                Apple Arcade
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                Apple Podcasts
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                Apple Books
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                Apple Store
+              </a>
+            </li>
+          </ul>
         </li>
         <li className="navbarItem">
           <a href="#">
             Accessory
           </a>
+          <ul>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                Accessory 모두보기
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                Mac
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                iPad
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                iphone
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                Apple Watch
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                AirPods
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                TV 및 홈
+              </a>
+            </li>
+          </ul>
         </li>
         <li className="navbarItem">
           <a href="#">
             Customer Support
           </a>
+          <ul>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                iphone
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                Mac
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                iPad
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                Watch
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                AirPods
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                Music
+              </a>
+            </li>
+            <li className="navbarItemInner">
+              <a href="#" className="cartegory">
+                TV
+              </a>
+            </li>
+          </ul>
         </li>
         <li className="navbarItem">
           <a href="#">
